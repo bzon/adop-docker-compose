a2c5a42cff335c375fd5e92be7da5b1c15220fd8
root <root@ip-172-31-41-107.us-west-2.compute.internal>
2016-08-05 17:51:07 +0000

    removed gerrit relations..

--- compose/mail-server/setup_mailserver.sh
+++ compose/mail-server/setup_mailserver.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
+rm -fr mail-config
+
 mkdir -p mail-config
 
 docker run --rm -e MAIL_USER=${INITIAL_ADMIN_USER}@${LDAP_DOMAIN} -e MAIL_PASS=${INITIAL_ADMIN_PASSWORD_PLAIN} tvial/docker-mailserver:v2 /bin/sh -c 'echo "$MAIL_USER|$(doveadm pw -s SHA512-CRYPT -u $MAIL_USER -p $MAIL_PASS)"' >> mail-config/postfix-accounts.cf
